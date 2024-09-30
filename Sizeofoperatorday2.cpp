3991437a35b01e19e0ced87ce1606aa30cf1d5b1
Codersharmajiyash <yashsharma78@outlook.com>
2024-09-30 18:57:48 +0530

    Add files via upload

---
+++ Sizeofoperatorday2.cpp
@@ -0,0 +1,13 @@
+SIZE OF OPERATOR
+//It determines the size in bytes of data typesand
+//variables,class,objects etc.
+#include <iostream>
+int main()
+{
+std::string name = "yash sharma";
+double cgpa = 2.5;
+char grade = 'F';
+bool student = true;
+std::cout << sizeof(name) << "bytes\n";
+}
+return 0;
