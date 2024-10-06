e5a4f01d544f512641302910c9d3a4c12bb82f00
Codersharmajiyash <yashsharma78@outlook.com>
2024-10-06 20:45:39 +0530

    Create Dayscalculatehypontenuse.cpp

---
+++ Dayscalculatehypontenuse.cpp
@@ -0,0 +1,37 @@
+
+#include<iostream>
+
+#include <cmath>
+
+intmain() {
+
+double a;
+
+#include<iostream>
+
+#include <cmath>
+
+double b;
+
+double c;
+
+std::cout << "Enter side A: ";
+
+std::cin >> a:;
+
+std::cout << "Enter side b:"; std::cin >> b::
+
+a = pow(a,2)
+
+b = pow(b,2
+
+c = sqrt(a + b)
+
+std::cout <<"sid c" << c;
+
+return 0;
+//another way would be c = sqrt(pow(a,2)+pow(b,2));
+
+std::cout << "sidec: " << c; }
+
+return 0;
