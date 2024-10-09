8841aa57543c29cba6d4e5b01b2374ddbf88d406
Codersharmajiyash <yashsharma78@outlook.com>
2024-10-09 22:47:22 +0530

    Create Day12gradeassigment.cpp

---
+++ Day12gradeassigment.cpp
@@ -0,0 +1,31 @@
+intmain() {
+
+char grade;
+
+std::cout <<"what letter grade?";
+
+std::cout <<grade;
+
+case 'A':
+
+std::cout << "great"; break;
+
+case 'B':
+
+std::cout << "very good"; break;
+
+case 'C':
+
+std::cout << "good"; break;
+
+case 'D':
+
+std::cout << "needs improvements";
+
+case 'f':
+
+break;
+
+std::cout << "fail";
+
+break;
