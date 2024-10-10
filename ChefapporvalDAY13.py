a58bf24ff33557740b64621a3bd2402e16262f54
Codersharmajiyash <yashsharma78@outlook.com>
2024-10-10 22:03:09 +0530

    Add files via upload

---
+++ ChefapporvalDAY13.py
@@ -0,0 +1,16 @@
+def is_expert(X, Y):
+    
+    if Y >= X / 2:
+        return "Munchy is an expert."
+    else:
+        return "Munchy is not an expert."
+
+# Example usage
+X = int(input("Enter the number of problems submitted (X): "))
+Y = int(input("Enter the number of problems approved (Y): "))
+
+result = is_expert(X, Y)
+print(result)
+
+
+    
