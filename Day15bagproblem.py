e2e4b7760cb4ca3b7009c1d1419b6054f2532819
Codersharmajiyash <yashsharma78@outlook.com>
2024-10-12 21:22:20 +0530

    Add files via upload

---
+++ Day15bagproblem.py
@@ -0,0 +1,17 @@
+def min_bags_needed(N, K, M):
+    
+    capacity_per_bag = K * M
+    
+    bags_needed = (N + capacity_per_bag - 1) // capacity_per_bag  # This ensures rounding up
+    
+    return bags_needed
+
+N = 100  
+K = 5   
+M = 10  
+
+result = min_bags_needed(N, K, M)
+print("Minimum number of bags needed:", result)
+
+
+    
