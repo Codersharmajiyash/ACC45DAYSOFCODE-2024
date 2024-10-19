5ca874f8595bb951e25d2755001e4d008a12f461
Codersharmajiyash <yashsharma78@outlook.com>
2024-10-19 22:39:06 +0530

    Add files via upload

---
+++ Day17EvenOddprogram.c.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<math.h>
+
+// programm to print even numbers from 0 to 100
+int main(){
+
+int i,x;
+
+/for(i = 0;i <= 100;i++){
+   if(i %2 ==0){
+       printf("%d\n",i);  
+          }
+       
+       }
+ //for all odd numbers from 0 to 100
+ for(i = 0;i <= 100;i++){
+  if(i %2 !=0){
+        printf("%d\n",i);
+     }
+ }
+
+
+
+
+
+    return 0;
+}
